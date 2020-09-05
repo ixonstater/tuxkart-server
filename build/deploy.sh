4ba6dfc48d6dd745973971544a6c19375913875c
Joshua Jarvis <joshuajarvis0711@gmail.com>
2020-09-05 14:40:05 -0600

    Migrated to one build-image.yml file, updated deployment script, added test docker compose.

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 #Change dir to app build dir
-cd /home/ixonstater/code/super-tux/ais
+cd /home/ixonstater/code/super-tux
 
 #Build images
 docker-compose -f build-image.yml build
-cd ../server
-docker-compose -f build-image.yml build
-cd ../build
 
 #Save images to docker hub
 docker push ixonstater/stkai:latest
--- build/load.sh
+++
@@ -1,6 +0,0 @@
-docker-compose down
-docker image rm supertuxserver
-docker image rm supertuxais
-docker load<supertuxais.tar
-docker load<supertuxserver.tar
-docker-compose up -d
